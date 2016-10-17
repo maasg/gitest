1bc436a1cc421e329a5ac4bdcecadbe7c634cab8
Gerard Maas <gerard.maas@gmail.com>
2016-10-17 17:46:03 +0200

    stable

--- churnified/svc/com.datafellas.g3nerator.ModelOutput-0/server/src/main/scala/Main.scala
+++
@@ -1,4 +0,0 @@
-package com.example.churnified_com.datafellas.g3nerator.modeloutput_0
-object Main extends App {
-  val s = com.example.churnified_com.datafellas.g3nerator.modeloutput_0.server.impl.Server
-}
