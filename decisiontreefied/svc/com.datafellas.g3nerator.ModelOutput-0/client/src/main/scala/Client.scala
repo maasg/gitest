74b03965ab69acb664fe733c804cf3bf9ef9cbdd
Gerard Maas <gerard.maas@gmail.com>
2016-11-03 20:24:21 +0100

    Added generated project decisiontreefied

--- decisiontreefied/svc/com.datafellas.g3nerator.ModelOutput-0/client/src/main/scala/Client.scala
+++ decisiontreefied/svc/com.datafellas.g3nerator.ModelOutput-0/client/src/main/scala/Client.scala
@@ -64,7 +64,7 @@ val ServiceToCatalog : () => Try[String] = () => {
     askServiceToCatalog().get //force failure case
   }
 
-  def from(host:String, port:Int=8882) = {
+  def from(host:String, port:Int=59677) = {
     val transport = new NettyTransceiver(new InetSocketAddress(host, port))
     val client = SpecificRequestor.getClient(classOf[com.example.decisiontreefied_com.datafellas.g3nerator.modeloutput_0.server.Methods], transport)
     transport -> client
