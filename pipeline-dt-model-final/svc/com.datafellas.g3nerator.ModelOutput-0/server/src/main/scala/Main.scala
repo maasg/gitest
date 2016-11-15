60e23186eb689068799195c620e412174fae5d63
Gerard Maas <gerard.maas@gmail.com>
2016-11-15 17:08:52 +0100

    clean slate

--- pipeline-dt-model-final/svc/com.datafellas.g3nerator.ModelOutput-0/server/src/main/scala/Main.scala
+++
@@ -1,6 +0,0 @@
-package com.example.pipeline_dt_model_final_com.datafellas.g3nerator.modeloutput_0
-object Main {
- def main(args: Array[String]): Unit = {
-   val s = com.example.pipeline_dt_model_final_com.datafellas.g3nerator.modeloutput_0.server.impl.Server
- }
-}
