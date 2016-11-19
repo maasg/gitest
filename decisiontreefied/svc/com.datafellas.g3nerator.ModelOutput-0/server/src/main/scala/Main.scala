116e2d5acd027e43f10a7f9f8ebabf36c19683c8
Gerard Maas <gerard.maas@gmail.com>
2016-11-19 11:43:36 +0100

    cleanup

--- decisiontreefied/svc/com.datafellas.g3nerator.ModelOutput-0/server/src/main/scala/Main.scala
+++
@@ -1,6 +0,0 @@
-package io.kensu.decisiontreefied_com.datafellas.g3nerator.modeloutput_0
-object Main {
- def main(args: Array[String]): Unit = {
-   val s = io.kensu.decisiontreefied_com.datafellas.g3nerator.modeloutput_0.server.impl.Server
- }
-}
