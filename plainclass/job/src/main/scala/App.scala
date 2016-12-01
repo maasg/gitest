72803b7e0826701daf9c52a99f94fea1853f2021
Gerard Maas <gerard.maas@gmail.com>
2016-12-01 17:27:33 +0100

    Added generated project plainclass

---
+++ plainclass/job/src/main/scala/App.scala
@@ -0,0 +1,76 @@
+
+package io.kensu
+/**
+  Outputs
+  -------
+  > 
+
+ */
+object Main {
+
+def main(args:Array[String]):Unit = {
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
+import com.typesafe.config._
+import scala.collection.JavaConverters._
+
+// Create spark configuration holder
+val sparkConf = new SparkConf()
+
+// Set configuration
+val config = ConfigFactory.load()
+val sparkConfig = config.getConfig("spark")
+                        .atPath("spark").entrySet.asScala.map(e => e.getKey -> config.getString(e.getKey))
+
+sparkConf.setAll(sparkConfig)
+
+sparkConf.setMaster(sparkConf.get("spark.master", "local[*]"))
+sparkConf.set("spark.app.name", sparkConf.get("spark.app.name", "plainclass"))
+
+// Set project Jars
+
+val libDir = new java.io.File(s"/usr/share/plainclass", "lib")
+val currentProjectJars = Array[String]( "io.kensu-plainclass.plainclass-0.0.1-SNAPSHOT.jar" ).map{j => new java.io.File(libDir, j).getAbsolutePath}
+val sparkLibDir = new java.io.File(s"/usr/share/plainclass", "spark-lib")
+val fromProjectJars = Array[String]().map{j => new java.io.File(sparkLibDir, j).getAbsolutePath}
+val jarsArray = (sparkConf.get("spark.jars", "").split(",").toArray ++ currentProjectJars ++ fromProjectJars).distinct.filter(!_.isEmpty)
+println("Add Jars: \n" + jarsArray.mkString("\n"))
+sparkConf.setJars(jarsArray)
+
+
+
+// Create Spark Context
+val sparkContext = new SparkContext(sparkConf)
+println("Debug Conf:" + sparkContext.getConf.toDebugString)
+
+// Spark Context alias
+val sc = sparkContext
+
+
+
+
+
+
+  def getStringConfig(path: String) : Option[String] = {
+    if (config.hasPath(path)) {
+      Some(config.getString(path))
+    } else {
+      None
+    }
+  }
+
+  val adalogUrl: Option[String] = getStringConfig("adalog.url")
+  val adalogUser: Option[String] = getStringConfig("adalog.authentication.username")
+  val adalogPassword: Option[String] = getStringConfig("adalog.authentication.password")
+
+
+// no custom variables 
+/* -- Code Cell: Some(6F321124115241698BDB36F9E85B28F7) -- */
+
+sparkContext.stop
+
+
+}
+}
---
+++ plainclass/job/src/main/scala/Classes.scala
@@ -0,0 +1,10 @@
+
+package io.kensu
+
+
+// no custom variables 
+/* -- Code Cell: Some(DAA25F84F87549899C4514E0A9DED55C) -- */ 
+
+  case class SingleClass(code: Integer)
+
+
