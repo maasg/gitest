74ef773b03bed523bfe3217ab6c886f58d6f4e90
Gerard Maas <gerard.maas@gmail.com>
2016-12-08 09:59:42 +0100

    Added generated project test-dash

--- test-dash/job/src/main/scala/Classes.scala
+++ test-dash/job/src/main/scala/Classes.scala
@@ -3,12 +3,6 @@ package io.kensu.testdash
 
 
   //---//
-
-val customVars = config.getConfig("notebook.custom.vars")
-// custom variables
-  val rootDir = customVars.getString("rootDir") 
-
-     
 /* -- Code Cell: Some(EFB96FCC9F1B471FA8A694C7F4D57649) -- */ 
 
   case class ModelDefinitionVersion(version: Int)
